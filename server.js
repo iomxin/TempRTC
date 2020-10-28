def882d7b0480d2dcc77e162c474dad0864f1f59
iomxin <aomsin254161@gmail.com>
2020-10-28 15:07:59 +0700

    Edit index.jade

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 var express = require('./config/express')
+var mongoose = require('./config/mongoose')
+var db=mongoose();
 var app =express();
 
 
